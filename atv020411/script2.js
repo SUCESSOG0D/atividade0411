fb9ac243a0c697e734fb324e981401270d2afeed
Arthur Gaspar Pereira <163169090+SUCESSOG0D@users.noreply.github.com>
2025-11-04 20:40:04 -0300

    Add files via upload

---
+++ atv020411/script2.js
@@ -0,0 +1,38 @@
+// Função para somar usando 'var'
+function calcularComVar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var soma = num1 + num2;
+  
+    if (isNaN(soma)) {
+      document.getElementById("resultado").innerHTML = "Por favor, insira números válidos.";
+    } else {
+      document.getElementById("resultado").innerHTML = "Resultado com var: " + soma;
+    }
+  }
+  
+  // Função para somar usando 'let'
+  function calcularComLet() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let soma = num1 + num2;
+  
+    if (isNaN(soma)) {
+      document.getElementById("resultado").innerHTML = "Por favor, insira números válidos.";
+    } else {
+      document.getElementById("resultado").innerHTML = "Resultado com let: " + soma;
+    }
+  }
+  
+  // Função para somar usando 'const'
+  function calcularComConst() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const soma = num1 + num2;
+  
+    if (isNaN(soma)) {
+      document.getElementById("resultado").innerHTML = "Por favor, insira números válidos.";
+    } else {
+      document.getElementById("resultado").innerHTML = "Resultado com const: " + soma;
+    }
+  }
